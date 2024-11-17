e3f5623d66d34d7f90c70665cf3fc4a31f1064c7
avivilloz <avivilloz@gmail.com>
2024-11-17 09:17:27 +0200

    ur

--- setup_github_ssh_key.sh
+++ setup_github_ssh_key.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]
+if [ $# -ne 3 ]
 then
-    echo "provide username[1] (only needed to name the file), email[2] (important) and host[3] (important. host example: github, bitbucket, etc)"
+    echo "provide keylabel[1] (only needed to name the file), email[2] (important) and host[3] (important. host example: github.com, bitbucket.org, etc)"
 	exit 1
 fi
 
-username=$1
+keylabel=$1
 email=$2
 host=$3
 
-identity_file=~/.ssh/$(host)_ssh_key_for_$(username)
+identity_file="~/.ssh/$keylabel"
 
 mkdir -p ~/.ssh
 ssh-keygen -t ed25519 -C "$email" -f "$identity_file"
 
 echo "\
-Host github.com
+Host $host
     User git
-    HostName github.com
+    HostName $host
     PreferredAuthentications publickey
     IdentityFile $identity_file"\
 >> ~/.ssh/config
